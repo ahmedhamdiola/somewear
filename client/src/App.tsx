0b952efd30c9400e05de71e2eb4d0fda8ada7059
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-04-23 14:45:11 +0200

    frontend structure init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import CartPage from './features/cart/pages/CartPage'
+import FavouritePage from './features/favourites/pages/FavouritePage'
+import HomePage from './features/common/pages/HomePage'
+import SignUpPage from './features/auth/pages/SignUpPage'
 function App() {
 
   return (
-    <div className="bg-red-400">HELLO</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<div className="bg-red-400">HELLO</div>} />
+          <Route path='/cart' element={<CartPage />} />
+          <Route path='/favourites' element={<FavouritePage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
